f3c5f7dc06a9b1a1f11353ad3bdfb3a71d2a908e
Timothy T. Joe <106974662+TimothyTJoe@users.noreply.github.com>
2024-04-20 22:43:32 +0000

    installed dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
-
+import ReactPlayer from "react-player";
+const url =
+  "https://stream-173.zeno.fm/cqqzg1637reuv";
 function App() {
-
   return (
     <>
       <h4>Hello Gee FM 104.7</h4>
+      <hr />
+      <ReactPlayer url={url} controls />
     </>
   );
 }
